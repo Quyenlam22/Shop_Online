1230f648ef7ee2eebc4b0a883fcfc29692543a80
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2025-05-19 14:59:58 +0700

    Create and set layout project

---
+++ src/layout/LayoutDefault/index.js
@@ -0,0 +1,46 @@
+import { NavLink, Outlet } from "react-router-dom";
+import './LayoutDefault.scss';
+import { IoMdHome } from "react-icons/io";
+import { FaCartShopping } from "react-icons/fa6";
+
+function LayoutDefault () {
+    const navLinkActive = (e) => {
+        return e.isActive ? "menu__link menu__link--active" : "menu__link";
+    }
+    // 20p
+    return (
+        <>
+            <div className="layout-default">
+                <header className="layout-default__header">
+                    <div className="layout-default__logo">
+                        <NavLink to="/" >
+                            <img src="/logo_shop.png" alt="Logo" />
+                        </NavLink>
+                    </div>
+                    <div className="menu">
+                        <ul>
+                            <li>
+                                <NavLink to="/" className={navLinkActive}>
+                                    Home <IoMdHome className="menu__icons"/>
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/cart" className={navLinkActive}>
+                                    Cart <FaCartShopping  className="menu__icons"/>
+                                </NavLink>
+                            </li>
+                        </ul>    
+                    </div>
+                </header> 
+                <main className="layout-default__main">
+                    <Outlet/>
+                </main>
+                <footer className="layout-default__footer">
+                    Copyright @ 2025 by Quyenn22 
+                </footer>
+            </div>
+        </>
+    );
+}
+
+export default LayoutDefault;
