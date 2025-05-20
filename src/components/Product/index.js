19615e8b0e0e283000d3e661669171b367ecb3e8
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2025-05-20 08:58:32 +0700

    Skeleton

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -3,13 +3,18 @@ import { getProductList } from "../../services/ProductService";
 import "./Product.scss";
 import ProductItem from "./ProductItem";
 
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
 function Product () {
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchApi = async () => {
             const result = await getProductList();
             setProducts(result);
+            setLoading(false);
         }
 
         fetchApi();
@@ -17,11 +22,26 @@ function Product () {
 
     return (
         <>
-            <div className="product__list">
-                {products.map(item => (
-                    <ProductItem item={item} key={item.id}/>
-                ))}
-            </div>
+            {loading ? (
+                <>
+                    <div className="product__list">
+                        {[...Array(30)].map((_, index) => (
+                            <div className="product__item" key={index}>
+                                <Skeleton className="product__image"/>
+                                <Skeleton />
+                                <Skeleton />
+                                <Skeleton />
+                            </div>
+                        ))}
+                    </div>
+                </>
+            ) : (
+                <div className="product__list">
+                    {products.map(item => (
+                        <ProductItem item={item} key={item.id}/>
+                    ))}
+                </div>
+            )}
         </>
     )
 }
